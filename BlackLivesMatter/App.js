22a2bb9f219d552e80be49b63accc77500c3b7c0
Dymon Moore <dymon.mooredcs@gmail.com>
2020-05-31 23:15:38 -0400

    Added navigation to the project.

--- BlackLivesMatter/App.js
+++ BlackLivesMatter/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, Text, View } from 'react-native';
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
