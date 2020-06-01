403fea311ec52920fc3105499fe566ec97155177
Dymon Moore <dymon.mooredcs@gmail.com>
2020-05-31 23:28:19 -0400

    Adding dependencies for navigation in project.

--- BlackLivesMatter/App.js
+++ BlackLivesMatter/App.js
@@ -3,6 +3,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './HomeScreen.js';
 
 
 const Stack = createStackNavigator();
